0ce6c9e0120ed5a97d086e9a90a325b07c06efe4
Bowie <lfranckx@thinkful.com>
2021-03-22 15:15:04 -0500

    sinstall dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import './styles/App.scss';
 
-function App() {
+import Footer from './components/Footer';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Router basename={process.env.PUBLIC_URL}>
+          {/* <Navbar /> */}
+            <Switch>
+              {/* <Route exact path="/" render={() => <Redirect to={'/home'} />} />
+              <Route path='/home' exact component={Home} /> */}
+            </Switch>
+        </Router>
+        <Footer />
+      </>
   );
 }
-
-export default App;
